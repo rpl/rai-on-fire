c230dbf6b532e13d77a7e9124e7ec8d3b51021b3
Luca Greco <luca.greco@alcacoop.it>
2009-12-22 16:20:02 +0100

    FIX+FEATURE: prevents multiple xmlhttprequest + downloading progress visual feedback

--- chrome/content/sidebar.js
+++ chrome/content/sidebar.js
@@ -32,24 +32,76 @@ function parse(xmlblock) {
     return RAITV;
 }
 
-function render(data) {
-    var iframe = document.getElementById("raionfire-videonav-iframe").contentWindow;  
-    iframe.postMessage(JSON.stringify(data), "*");  
+function showMessage(msg) {
+    var videonav_window = document.getElementById("raionfire-videonav-iframe").contentWindow;  
+    videonav_window.postMessage(JSON.stringify({ show_message: msg }), "*");
 }
 
+function hideMessage() {
+    var videonav_window = document.getElementById("raionfire-videonav-iframe").contentWindow;  
+    videonav_window.postMessage(JSON.stringify({ hide_message: true }), "*");
+}
+
+
+function render(data) {
+    var videonav_window = document.getElementById("raionfire-videonav-iframe").contentWindow;  
+    videonav_window.postMessage(JSON.stringify(data), "*");  
+}
+
+function transferCompleted(evt) {
+    showMessage("Caricamento del palinsesto RAI completato... buona visione!");
+    setTimeout(hideMessage, 3000);
+}
+
+function transferFailed(evt) {
+    showMessage("Impossibile scaricare il palinsesto RAI... controllare la connessione di rete.");
+}
+
+function transferAborted(evt) {
+    showMessage("Caricamento del palinsesto RAI annullato.");
+}
+
+function updateProgress(evt) {
+  if (evt.lengthComputable) {
+    var percentComplete = Math.ceil(evt.loaded / evt.total * 100);
+    showMessage("Caricamento in corso... "+percentComplete+"%");
+  } else {
+    showMessage("Caricamento in corso... impossibile determinarne la durata ");
+  }
+}
+
+
+
 function download() {
-    //clean iframe DOM...
-    document.getElementById("raionfire-videonav-iframe").contentDocument.location.reload();
-    var req = new XMLHttpRequest();
+    var download_button = document.getElementById("raionfire-download-button");
+    var videonav_iframe = document.getElementById("raionfire-videonav-iframe");
+
+    if(download_button.getAttribute("label") !== "Download") {
+      download.req.abort();
+      download_button.setAttribute("label", "Download");
+      return;
+    }	  
+    //NOTE: clean iframe DOM...
+    videonav_iframe.contentDocument.location.reload();
+    //NOTE: send http request
+    var req = download.req = new XMLHttpRequest();
+    req.addEventListener("progress", updateProgress, false);
+    req.addEventListener("load", transferCompleted, false);
+    req.addEventListener("error", transferFailed, false);
+    req.addEventListener("abort", transferAborted, false);
     req.open('GET', 'http://www.rai.tv/dl/RaiTV/videoWall/PublishingBlock-5566288c-3d21-48dc-b3e2-af7fbe3b2af8.xml', true);
     req.onreadystatechange = function onreadychange(aEvt) {
+        download_button.setAttribute("label", "caricamento... annullare?");
 	if (req.readyState == 4) {
+            download_button.setAttribute("label", "Download");
 	    if (req.status == 200) {
 		render(parse(req.responseXML));
 		//Application.console.log(req.responseText);
 	    }
-	    else
-		Application.console.log("Error loading page\n");
+	    else {
+	        render_error_loading();
+		Application.console.log("ERROR - raionfire: Error loading page\n");
+            }			       
 	}
 	
     }
